94d84ccdd689141c61ec7e9e6558e03cd83a9087
Masoud Ghanbarzadeh <33165677+ghmasood@users.noreply.github.com>
2025-09-02 01:45:21 +0330

    fix: some style

--- src/app/rules/page.tsx
+++ src/app/rules/page.tsx
@@ -4,13 +4,13 @@ import React from 'react';
 function Page() {
   return (
     <div className='min-h-[calc(100dvh_-_9rem)] bg-gray-50 p-4 max-w-md mx-auto rounded-2xl relative flex flex-col'>
-      <ol className='text-gray-600 list-[upper-roman] px-4'>
+      <ol className='text-gray-600 list-[upper-roman] flex flex-col gap-1 px-4'>
         <li className='text-justify'>
           اگر یک تیم تقلب کرد و تیم مقابل متوجه شد 165 به ضرر تیم متقلب است ،
           ولی تیمی که متوجه تقلب شده است می تواند به بازی ادامه دهد و تیم متقلب
           را شلم کند.
         </li>
-        <li>
+        <li className="text-justify">
           اگر تیم حریف رو دست پخش کننده بولی نزنه بعد از دیدن دست و قبل از خوندن امکان اینکه تیم حریف بگه مجدد دست بدین هستش
         </li>
       </ol>
